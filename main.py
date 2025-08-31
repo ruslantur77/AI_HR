e0506daae9c610b88793471f473166f6432f67d5
imbalet <zhmaevva@gmail.com>
2025-08-31 16:48:42 +0300

    feat: add exception handler

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from api import auth_router, static_router, webrtc_router
 from config import config
 from database import create_tables
+from exceptions import AppException
+from exceptions_handler import exception_handler
 from rtc.rtc import shutdown
 
 logging.basicConfig(level=logging.INFO)
@@ -46,4 +48,5 @@ app = FastAPI(
 app.include_router(webrtc_router)
 app.include_router(static_router)
 app.include_router(auth_router)
+app.add_exception_handler(AppException, exception_handler)
 app.mount("/static", StaticFiles(directory="static"), name="static")
