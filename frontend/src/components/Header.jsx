5d2fe341963c2cb5479da7416adb16dc60f67711
ruslantur77 <ruslantursunov77@gmail.com>
2025-09-05 21:35:26 +0300

    Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import './Header.css';
+import logo from '/src/assets/logo.png';
+import { useNavigate } from 'react-router-dom';
+
+export default function Header() {
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/');
+    };
+
+    return (
+        <header className="header">
+            <h1
+                className="clickable-title"
+                onClick={handleLogoClick}
+                style={{ cursor: 'pointer' }}
+            >
+                AI HR
+            </h1>
+            <div className="logo">
+                <img
+                    className="logo-image"
+                    src={logo}
+                    alt="Logo"
+                    onClick={handleLogoClick}
+                />
+            </div>
+        </header>
+    );
+}
