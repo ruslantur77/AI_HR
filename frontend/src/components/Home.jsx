1aa60d04790f7b8c2bc574b4edbf1846670c5505
ruslantur77 <ruslantursunov77@gmail.com>
2025-09-06 16:47:28 +0300

    some small improvements

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,9 +5,7 @@ export default function Home() {
   return (
     <main className="home">
       <h1 className="home__title">Собеседование с AI HR</h1>
-
       <AiButton />
-
     </main>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom' // ✅ исправлено
+import { createBrowserRouter, RouterProvider } from 'react-router-dom' 
 import './index.css'
 import App from './App.jsx'
 import Screening from './pages/Screening.jsx'
